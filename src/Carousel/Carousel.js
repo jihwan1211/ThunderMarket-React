efbe2e7eb0026fe02304cfa622692674b2e34b9e
jihwan1211 <jhwank1211@gmail.com>
2023-05-22 14:09:08 +0900

    styled

--- src/Carousel/Carousel.js
+++ src/Carousel/Carousel.js
@@ -1,12 +1,28 @@
 import "./Carousel.css";
+import { styled } from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const CarouselDiv = styled.div`
+  width: 1024px;
+`;
+
+const CarouselImg = styled.img`
+  width: 1024px;
+`;
 
 const Carousel = () => {
   return (
-    <div className="CarouselContainer">
-      <div className="Carousel">
-        <img src="https://media.bunjang.co.kr/images/nocrop/972479333_w4116.jpg"></img>
-      </div>
-    </div>
+    <Container>
+      <CarouselDiv>
+        <CarouselImg
+          src={"https://media.bunjang.co.kr/images/nocrop/972479333_w4116.jpg"}
+        ></CarouselImg>
+      </CarouselDiv>
+    </Container>
   );
 };
 
